ac67e3c1072f3b78c853af2122001dc515f0eea4
andluis35 <and.luis35@gmail.com>
2024-10-05 09:06:43 -0300

    Exercício 001 finalizado

---
+++ Exercicios/Exercicio001.java
@@ -0,0 +1,13 @@
+package exercicio001;
+
+/**
+ * @author Anderson Luis
+ */
+
+public class Exercicio001 {
+    
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+    
+}

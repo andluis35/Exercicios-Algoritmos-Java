c0345d834f5028f289c182b5cd5a66821b3d87a7
andluis35 <and.luis35@gmail.com>
2024-10-26 09:38:45 -0300

    Exercício 038 finalizado

---
+++ Exercicios/Exercicio038.java
@@ -0,0 +1,19 @@
+package exercicio038;
+
+/**
+ * @author Anderson Luis
+ */
+
+public class Exercicio038 {
+
+    public static void main(String[] args) {
+        int i = 6;
+        
+        while(i <= 11) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println("Acabou!");
+    }
+    
+}

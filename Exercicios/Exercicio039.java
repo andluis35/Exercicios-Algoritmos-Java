e363003c9cbfa6efac3ea09c58d4879f82db3269
andluis35 <and.luis35@gmail.com>
2024-10-27 08:28:19 -0300

    Exercício 039 finalizado

---
+++ Exercicios/Exercicio039.java
@@ -0,0 +1,19 @@
+package exercicio039;
+
+/**
+ * @author Anderson Luis
+ */
+
+public class Exercicio039 {
+
+    public static void main(String[] args) {
+        int i = 10;
+        
+        while(i >= 3) {
+            System.out.print(i + " ");
+            i--;
+        }
+        System.out.println("Acabou!");
+    }
+    
+}

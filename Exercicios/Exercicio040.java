ca46b9c75aec24f6a86cfa89b6c12f2b07cd874a
andluis35 <and.luis35@gmail.com>
2024-10-28 07:10:04 -0300

    Exercício 040 finalizado

---
+++ Exercicios/Exercicio040.java
@@ -0,0 +1,19 @@
+package exercicio040;
+
+/**
+ * @author Anderson Luis
+ */
+
+public class Exercicio040 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        
+        while(i <= 18) {
+            System.out.print(i + " ");
+            i += 3;
+        }
+        System.out.println("Acabou!");
+    }
+    
+}

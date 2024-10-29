15d1f5fb5197c6ef29f0f6aaf3d60c3656fb9144
andluis35 <and.luis35@gmail.com>
2024-10-29 17:17:26 -0300

    Exercício 041 finalizado

---
+++ Exercicios/Exercicio041.java
@@ -0,0 +1,19 @@
+package exercicio041;
+
+/**
+ * @author Anderson Luis
+ */
+
+public class Exercicio041 {
+
+    public static void main(String[] args) {
+        int i = 100;
+        
+        while(i >= 0) {
+            System.out.print(i + " ");
+            i -= 5;
+        }
+        System.out.println("Acabou!");
+    }
+    
+}

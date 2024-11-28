5639e0b251f27844dc1c716496e08636dc9fa0d3
andluis35 <and.luis35@gmail.com>
2024-11-28 07:13:53 -0300

    Exercício 061 finalizado

---
+++ Exercicios/Exercicio061.java
@@ -0,0 +1,20 @@
+package exercicio061;
+
+/**
+ * @author Anderson Luis
+ */
+
+public class Exercicio061 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        
+        do {
+            System.out.print(i + " ");
+            i += 3;
+        } while(i <= 30);
+        
+        System.out.println("Acabou!");
+    }
+    
+}

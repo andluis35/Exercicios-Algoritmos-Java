8308b2885c6a746ff9f8b4d29e877e1fd9d730fa
andluis35 <and.luis35@gmail.com>
2024-12-01 06:19:44 -0300

    Exercício 064 finalizado

---
+++ Exercicios/Exercicio064.java
@@ -0,0 +1,17 @@
+package exercicio064;
+
+/**
+ * @author Anderson Luis
+ */
+
+public class Exercicio064 {
+
+    public static void main(String[] args) {
+        
+        for(int i = 0; i <= 40; i += 5) {
+            System.out.print(i + " ");
+        }
+        System.out.println("Acabou!");
+    }
+    
+}

b870f9bb24a0daad3f03fc0561f7b1600f012cdd
andluis35 <and.luis35@gmail.com>
2024-12-02 07:48:54 -0300

    Exercício 065 finalizado

---
+++ Exercicios/Exercicio065.java
@@ -0,0 +1,17 @@
+package exercicio065;
+
+/**
+ * @author Anderson Luis
+ */
+
+public class Exercicio065 {
+
+    public static void main(String[] args) {
+        
+        for(int i = 100; i >= 0; i -= 10) {
+            System.out.print(i + " ");
+        }
+        System.out.println("Acabou!");
+    }
+    
+}

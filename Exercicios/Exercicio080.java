35265f5a07bbc29024fdb228e96bc4063b69c230
andluis35 <and.luis35@gmail.com>
2024-12-19 17:47:37 -0300

    Exercício 080 finalizado

--- Exercicios/Exercicio080.java
+++ Exercicios/Exercicio080.java
@@ -13,26 +13,16 @@ public class Exercicio080 {
         Scanner keyboard = new Scanner(System.in);
         int[] arrayOfNumbers = new int[30];
         int key;
-        int timesKeyWasFound = 0;
         
         arrayOfNumbers = fillArray(arrayOfNumbers);
         
         System.out.print("Digite um número (chave): ");
         key = keyboard.nextInt();
         
-        for(int value : arrayOfNumbers) {
-            System.out.print(value + " ");
-        }
-        
-        System.out.println("");
-        
-        for(int j = 0; j < arrayOfNumbers.length; j++) {
-            if(key == arrayOfNumbers[j]) {
-                System.out.print(j + " ");
-                timesKeyWasFound++;
-            }
-        }
-        System.out.println("");
+        System.out.println("---------------------------------------------------");
+        printArray(arrayOfNumbers);
+        System.out.println("---------------------------------------------------");
+        printIndexes(key, arrayOfNumbers);
 
     }
     
@@ -44,6 +34,39 @@ public class Exercicio080 {
         }
         
         return arrayOfNumbers;
+    }
+    
+    public static void printArray(int[] arrayOfNumbers) {
+        System.out.print("[ ");
+        
+        for(int i = 0; i < arrayOfNumbers.length; i++) {
+            if(isTheLastElement(i, arrayOfNumbers)) {
+                System.out.print(arrayOfNumbers[i] + " ");
+            }
+            else {
+                System.out.print(arrayOfNumbers[i] + " | ");
+            }  
+            
+        }
+        
+        System.out.println("]");
+    }
+    
+    public static boolean isTheLastElement(int i, int[] arrayOfNumbers) {
+        return i == arrayOfNumbers.length - 1;
+    }
+    
+    public static void printIndexes(int key, int[] arrayOfNumbers) {
+        int timesKeyWasFound = 0;
+        
+        System.out.print("O número " + key + " aparece na(s) posição(ões): ");
+        for(int j = 0; j < arrayOfNumbers.length; j++) {
+            if(key == arrayOfNumbers[j]) {
+                System.out.print(j + " ");
+                timesKeyWasFound++;
+            }
+        }
+        System.out.println("(um total de " + timesKeyWasFound + " vez(es)!)");
     }
     
 }

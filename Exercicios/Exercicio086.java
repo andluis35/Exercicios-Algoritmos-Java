6c245eef4dd8d879e819f0818fb6c1b82f6e0ee6
andluis35 <and.luis35@gmail.com>
2024-12-25 08:21:43 -0300

    Exercício 086 finalizado

---
+++ Exercicios/Exercicio086.java
@@ -0,0 +1,19 @@
+package exercicio086;
+
+/**
+ * @author Anderson Luis
+ */
+
+public class Exercicio086 {
+
+    public static void main(String[] args) {
+        gerador();
+    }
+    
+    public static void gerador() {
+        System.out.println("+-------=======-------+");
+        System.out.println("      Olá, Mundo!");
+        System.out.println("+-------=======-------+");
+    }
+    
+}

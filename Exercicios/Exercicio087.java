b15c78de5e754683346c88945faf0006de98444b
andluis35 <and.luis35@gmail.com>
2024-12-26 07:19:01 -0300

    Exercício 087 finalizado

---
+++ Exercicios/Exercicio087.java
@@ -0,0 +1,19 @@
+package exercicio087;
+
+/**
+ * @author Anderson Luis
+ */
+
+public class Exercicio087 {
+
+    public static void main(String[] args) {
+        gerador("Aprendendo Java");
+    }
+    
+    public static void gerador(String message) {
+        System.out.println("+-------=======-------+");
+        System.out.println("    " + message);
+        System.out.println("+-------=======-------+");
+    }
+    
+}
